8c4e5c7f932692697e7d0152c212c1b164359f21
Unknown <twynsicle@gmail.com>
2019-10-28 12:31:51 +1300

    add api to azure and update app to use production url

--- app/companion/index.js
+++ app/companion/index.js
@@ -29,21 +29,18 @@ messaging.peerSocket.onopen = function() {
     poll();
 };
 
-messaging.peerSocket.onmessage = function(evt) {
-};
+messaging.peerSocket.onmessage = function(evt) {};
 
-messaging.peerSocket.onerror = function(err) {
-};
+messaging.peerSocket.onerror = function(err) {};
 
 function poll() {
     retrieveAndSendSchedule();
     setTimeout(function() {
         if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
             //TODO This is a promise, do we want to run every 30 seconds or wait 30 seconds between polls.
-            retrieveAndSendSchedule()
-                .then(() => {
-                    poll();
-                });
+            retrieveAndSendSchedule().then(() => {
+                poll();
+            });
         }
         if (messaging.peerSocket.readyState === messaging.peerSocket.CLOSED) {
             console.log('companion closed');
@@ -52,33 +49,44 @@ function poll() {
 }
 
 function retrieveAndSendSchedule() {
-    return retrieveSchedule()
-        .then(response => {
-            //console.log('sending' + JSON.stringify(response));
-            messaging.peerSocket.send(response);
-        });
+    return retrieveSchedule().then(response => {
+        //console.log('sending' + JSON.stringify(response));
+        messaging.peerSocket.send(response);
+    });
 }
+
+//TODO does this always run, or only when the app is open?
 
 function retrieveSchedule() {
     //TODO temp until settings are added
-    settingsStorage.setItem(PLATFORM_SETTING, JSON.stringify({
-        platforms: [{
-            platformNumber: '10976',
-            image: 'left.jpg',
-        }, {
-            platformNumber: '10171',
-            image: 'right.jpg',
-        },
-        ],
-    }));
+    settingsStorage.setItem(
+        PLATFORM_SETTING,
+        JSON.stringify({
+            platforms: [
+                {
+                    platformNumber: '10976',
+                    image: 'left.jpg',
+                },
+                {
+                    platformNumber: '10171',
+                    image: 'right.jpg',
+                },
+            ],
+        })
+    );
 
     //TODO handle the no platforms configured instance.
 
-    let platformSettings = JSON.parse(settingsStorage.getItem(PLATFORM_SETTING));
+    let platformSettings = JSON.parse(
+        settingsStorage.getItem(PLATFORM_SETTING)
+    );
 
-    let platformNumbers = platformSettings.platforms.map(ps => ps.platformNumber);
+    let platformNumbers = platformSettings.platforms.map(
+        ps => ps.platformNumber
+    );
 
-    return metroApi.getDeparturesAtStop(platformNumbers)
+    return metroApi
+        .getDeparturesAtStop(platformNumbers)
         .then(function(apiResponse) {
             let merged = [];
 
@@ -87,7 +95,9 @@ function retrieveSchedule() {
                     platformNumber: platformSetting.platformNumber,
                 };
 
-                let platformResponse = apiResponse.platforms.find(r => r.platformNumber === platformSetting.platformNumber);
+                let platformResponse = apiResponse.platforms.find(
+                    r => r.platformNumber === platformSetting.platformNumber
+                );
                 if (platformResponse) {
                     result.name = platformResponse.name;
                     result.trips = platformResponse.trips;
--- app/companion/metro-api.js
+++ app/companion/metro-api.js
@@ -1,11 +1,19 @@
+const debugUrlBase = 'http://localhost:7071/api/GetTripsForPlatform?platforms=';
+const productionUrlBase = 'https://christchurchmetro.azurewebsites.net/api/GetTripsForPlatform?platforms=';
+
 export function MetroApi() {
 
 }
 
 MetroApi.prototype.getDeparturesAtStop = function(platformNumbers) {
-    console.log('MetroApi.getDeparturesAtStop');
+    if (platformNumbers == null || platformNumbers.length === 0) {
+        return new Promise(function(resolve, reject) {
+            resolve([]);
+        })
+    }
+
     return new Promise(function(resolve, reject) {
-        let url = `http://localhost:7071/api/GetTripsForPlatform?platforms=${platformNumbers.join(',')}`;
+        let url = productionUrlBase + platformNumbers.join(',');
 
         console.log(`fetching: ${url}`);
         fetch(url).then(function(response) {
